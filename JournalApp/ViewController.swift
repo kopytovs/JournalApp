50a9041eb677b7292efa12f79830f8064ea16082
kopytovs <kopytov@me.com>
2017-12-10 19:02:49 +0300

    initial commit

---
+++ JournalApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  JournalApp
+//
+//  Created by Sergey Kopytov on 10.12.2017.
+//  Copyright © 2017 Sergey Kopytov. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# JournalApp
+
+A description of this package.
